bd345021856e4daa37ecfd1cd3ac53306611e976
cqsd <>
2017-01-17 02:35:16 -0600

    take out some trash

--- src/chipper/core.cljs
+++ src/chipper/core.cljs
@@ -8,38 +8,14 @@
 
 (enable-console-print!)
 
-(def app-state
-  (r/atom
-    (let [context (a/create-audio-context)]
-      {:context  context
-       :sources {:square   (a/create-osc context "square" :A)
-                 :triangle (a/create-osc context "triangle" :C 5)
-                 :sawtooth (a/create-osc context "sawtooth" :A 3)}})))
-
-; (def track
-;   [[[:A 4 nil nil] [:C 4 nil nil]]
-;    [[:B 4 nil nil] [:D 4 nil nil]]
-;    [[:C 5 nil nil] [:E 4 nil nil]]
-;    [[:D 5 nil nil] [:F 4 nil nil]]
-;    [[:E 5 nil nil] [:G 4 nil nil]]
-;    [[:F 5 nil nil] [:A 4 nil nil]]
-;    [[:F 5 nil nil] [:B 4 nil nil]]
-;    [[:E 5 nil nil] [:C 5 nil nil]]])
-
 (def track
   (for [_ (range 100)]
    [[:A nil nil nil] [:C 4 nil nil]]))
 
-(def event-chan (r/atom (k/init-keydown-chan!)))
+(def schema [:square :triangle])
 
 (defn on-js-reload []
   (r/render-component
     [:div.container
-     [ui/tracker ui/schema track]
-     ;[ui/ex-player-2 app-state track]
-     ]
-    (.getElementById js/document "hack"))
-
-  (go-loop [x (<! @event-chan)]
-    (prn (keyword x))
-    (recur (<! @event-chan))))
+     [ui/tracker schema track]]
+    (.getElementById js/document "hack")))

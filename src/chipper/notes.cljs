2b3558b471c7f7fe64a459e9371257038018eebf
cqsd <>
2018-03-03 05:33:33 -0600

    temp, will revisit this commit message when awake

--- src/chipper/notes.cljs
+++ src/chipper/notes.cljs
@@ -1,10 +1,14 @@
 (ns chipper.notes)
 
-(def ;; Half-steps above middle C. This is easier than, e.g., indexing.
-  note-steps
+(defrecord Note [note octave gain effect])
+
+;; half-steps above C
+;; TODO might as well just shove em in a vector because I don't intend
+;; to add any facility for enharmonics in the editor
+(def note-steps
   {:C  0
-   :C# 1  :Db 1 ;; it's just easier to do this sort of thing than it is to
-   :D  2        ;; use, e.g., a translation table for accidentals
+   :C# 1  :Db 1
+   :D  2
    :D# 3  :Eb 3
    :E  4
    :F  5
@@ -15,36 +19,32 @@
    :A# 10 :Bb 10
    :B  11 :Ab 11})
 
-(def twelfth-root-of-2 1.059) ;; only realistically need this much precision
+;; realistically don't need this much precision
+(def twelfth-root-of-2 1.059)
 ;; (def twelfth-root-of-2 (js/Math.pow 2 (/ 1 12)))
 
-;; Unused
-; (defn- round-to-two-decs [decimal]
-;   (float (/ (js/Math.round (* 100 decimal)) 100)))
-
 (defn frequency-
-  "Calculate the frequency of a note given the note name, octave number,
-  and frequency of A4.
+  {:pre [(note-steps note)]
+   :doc
+   "Calculate the frequency of a note given the note name, octave number,
+   and frequency of A4.
 
-  Returns:
+   Returns:
    - frequency (Hz)
 
-  Params:
+   Params:
    - note - name of note; # for sharp, b for flat
    - octave - octave number (default 4)
    - base - base frequency of A4 (default 440)
 
-  Reference: http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html"
+   Reference: http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html"}
 
-  ;; TODO for this use it's probably not necessary to have this variadic
-  ([note]        (frequency- note 4))
-  ; ([note octave] (frequency note octave 440))
-  ([note octave]
-   (let [freq-of-a4    440
-         steps-from-c4 (+ (* (- octave 4) 12)
-                          (note note-steps)
-                          -9)] ;; base freq given for A4 but steps calc'd from C4
-     (* freq-of-a4
-        (js/Math.pow twelfth-root-of-2 steps-from-c4)))))
+  [note octave]
+  (let [freq-of-a4    440
+        steps-from-c4 (+ (* (- octave 4) 12)
+                         (note-steps note)
+                         -9)] ;; base freq given for A4 but steps calc'd from C4
+    (* freq-of-a4
+       (js/Math.pow twelfth-root-of-2 steps-from-c4))))
 
 (def frequency (memoize frequency-))

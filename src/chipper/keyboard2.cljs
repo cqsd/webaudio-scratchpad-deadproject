3ac624703761c8f22c30c14baaee0d28a137b126
cqsd <colloquiasd@gmail.com>
2018-06-19 22:26:05 -0700

    soon!

---
+++ src/chipper/keyboard2.cljs
@@ -0,0 +1,6 @@
+(ns chipper.keyboard2 ; electric booga--[being dragged away by the deep state]
+  (:require [chipper.chips :as c]
+            [chipper.utils :as u]
+            [goog.events :as events]))
+
+;; gonna migrate to this file and deleet original later
